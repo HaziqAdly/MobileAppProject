053cc3000093f75c322f6ee819d712ce41ba2f28
Haziq Adli 1814981 <55783257+HaziqAdly@users.noreply.github.com>
2022-02-01 16:55:15 +0800

    Update auth_form.dart

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -71,11 +71,13 @@ class _AuthFormState extends State<AuthForm> {
                   Container(
                     padding: const EdgeInsets.fromLTRB(15.0, 125.0, 0.0, 0.0),
                     child: const Text('Jokes',
-                        style: TextStyle(
-                            color: Color.fromRGBO(253, 111, 150, 1),
-                            fontSize: 80.0,
-                            fontFamily: "Raleway",
-                            fontWeight: FontWeight.bold)),
+                      style: TextStyle(
+                        color: Color.fromRGBO(253, 111, 150, 1),
+                        fontSize: 80.0,
+                        fontFamily: "Raleway",
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
                   ),
                 ],
               ),
@@ -83,153 +85,163 @@ class _AuthFormState extends State<AuthForm> {
                 key: _formKey,
                 child: SingleChildScrollView(
                   child: Container(
-                      padding: const EdgeInsets.only(
-                          top: 54.0, left: 20.0, right: 30.0),
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            key: const ValueKey('email'),
-                            decoration: const InputDecoration(
-                                labelText: 'EMAIL',
-                                labelStyle: TextStyle(
-                                    fontFamily: 'RobotoCondensed',
+                    padding: const EdgeInsets.only(
+                      top: 54.0, left: 20.0, right: 30.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          key: const ValueKey('email'),
+                          decoration: const InputDecoration(
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                              fontFamily: 'RobotoCondensed',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green),
+                            )
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                          enableSuggestions: false,
+                          validator: (value) {
+                            if (value.isEmpty || !value.contains('@')) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _userEmail = value;
+                          },
+                        ),
+                        if (!_isLogin) const SizedBox(height: 20.0),
+                        if (!_isLogin)
+                        TextFormField(
+                          key: const ValueKey('username'),
+                          decoration: const InputDecoration(
+                            labelText: 'USERNAME',
+                            labelStyle: TextStyle(
+                              fontFamily: 'RobotoCondensed',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                BorderSide(color: Colors.green)
+                            )
+                          ),
+                          autocorrect: true,
+                          textCapitalization: TextCapitalization.words,
+                          enableSuggestions: false,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter a username';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _userName = value;
+                          },
+                          // ignore: missing_return
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          key: const ValueKey('password'),
+                          decoration: const InputDecoration(
+                            labelText: 'PASSWORD',
+                            labelStyle: TextStyle(
+                              fontFamily: 'RobotoCondensed',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey
+                            ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green)),
+                          ),
+                          obscureText: true,
+                          // ignore: missing_return
+                          validator: (value) {
+                            if (value.isEmpty || value.length < 7) {
+                              return 'Please enter a long password';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _userPassword = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        if (widget.isLoading)
+                        const CircularProgressIndicator(),
+                        if (!widget.isLoading)
+                        Container(
+                          height: 57.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.black,
+                            color: const Color.fromRGBO(111, 105, 172, 1),
+                            elevation: 10.0,
+                            child: TextButton(
+                              onPressed: _trySubmit,
+                              child: Center(
+                                child: Text(
+                                  _isLogin ? "Login" : "Sign up",
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 23,
                                     fontWeight: FontWeight.bold,
-                                    color: Colors.grey),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.green),
-                                )),
-                            keyboardType: TextInputType.emailAddress,
-                            autocorrect: false,
-                            textCapitalization: TextCapitalization.none,
-                            enableSuggestions: false,
-                            validator: (value) {
-                              if (value.isEmpty || !value.contains('@')) {
-                                return 'Please enter a valid email address';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) {
-                              _userEmail = value;
-                            },
-                          ),
-                          if (!_isLogin) const SizedBox(height: 20.0),
-                          if (!_isLogin)
-                            TextFormField(
-                              key: const ValueKey('username'),
-                              decoration: const InputDecoration(
-                                  labelText: 'USERNAME',
-                                  labelStyle: TextStyle(
-                                      fontFamily: 'RobotoCondensed',
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey),
-                                  focusedBorder: UnderlineInputBorder(
-                                      borderSide:
-                                          BorderSide(color: Colors.green))),
-                              autocorrect: true,
-                              textCapitalization: TextCapitalization.words,
-                              enableSuggestions: false,
-                              validator: (value) {
-                                if (value.isEmpty) {
-                                  return 'Please enter a username';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                _userName = value;
-                              },
-                              // ignore: missing_return
-                            ),
-                          const SizedBox(height: 20.0),
-                          TextFormField(
-                            key: const ValueKey('password'),
-                            decoration: const InputDecoration(
-                              labelText: 'PASSWORD',
-                              labelStyle: TextStyle(
-                                  fontFamily: 'RobotoCondensed',
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey),
-                              focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.green)),
-                            ),
-                            obscureText: true,
-
-                            // ignore: missing_return
-                            validator: (value) {
-                              if (value.isEmpty || value.length < 7) {
-                                return 'Please enter a long password';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) {
-                              _userPassword = value;
-                            },
-                          ),
-                          const SizedBox(
-                            height: 35,
-                          ),
-                          if (widget.isLoading)
-                            const CircularProgressIndicator(),
-                          if (!widget.isLoading)
-                            Container(
-                              height: 57.0,
-                              child: Material(
-                                borderRadius: BorderRadius.circular(20.0),
-                                shadowColor: Colors.black,
-                                color: const Color.fromRGBO(111, 105, 172, 1),
-                                elevation: 10.0,
-                                child: TextButton(
-                                  onPressed: _trySubmit,
-                                  child: Center(
-                                    child: Text(
-                                      _isLogin ? "Login" : "Sign up",
-                                      style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 23,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'Raleway'),
-                                    ),
+                                    fontFamily: 'Raleway'
                                   ),
                                 ),
                               ),
                             ),
-                          const SizedBox(
-                            height: 25,
                           ),
-                          if (!widget.isLoading)
-                            Container(
-                              height: 57.0,
-                              child: Material(
-                                borderRadius: BorderRadius.circular(40.0),
-                                color: Colors.white,
-                                child: TextButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _isLogin = !_isLogin;
-                                    });
-                                  },
-                                  child: Center(
-                                    child: Text(
-                                      _isLogin
-                                          ? "Create new account"
-                                          : "I already have an account",
-                                      style: const TextStyle(
-                                          color:
-                                              Color.fromRGBO(253, 111, 150, 1),
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'Raleway'),
-                                    ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        if (!widget.isLoading)
+                        Container(
+                          height: 57.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(40.0),
+                            color: Colors.white,
+                            child: TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Center(
+                                child: Text(
+                                  _isLogin
+                                    ? "Create new account"
+                                    : "I already have an account",
+                                  style: const TextStyle(
+                                    color:
+                                      Color.fromRGBO(253, 111, 150, 1),
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Raleway'
                                   ),
                                 ),
                               ),
                             ),
-                          Padding(
-                              padding: EdgeInsets.only(
-                                  bottom: MediaQuery.of(context)
-                                      .viewInsets
-                                      .bottom)),
-                        ],
-                      )),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context)
+                            .viewInsets
+                            .bottom
+                          )
+                        ),
+                      ],
+                    )
+                  ),
                 ),
               ),
             ],
